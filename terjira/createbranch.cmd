88f3a68e519819f0ed57cf166965d10272b4c2ad
Walt Hengely <whengely@igsenergy.com>
2018-08-06 11:33:39 -0400

    now works with BRANCHPREFIX variable

--- terjira/createbranch.cmd
+++ terjira/createbranch.cmd
@@ -1,5 +1,6 @@
 @ECHO OFF
-
+IF "%BRANCHPREFIX%" == "" GOTO NOBRANCHPREFIX
+:YESBRANCHPREFIX
 for /f "tokens=* USEBACKQ" %%a in (
 `jira issue %1 ^| head -n 4 ^| tail -n 3 ^| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" ^|^
  tr -s " " ^| cut -c 5- ^| sed -e "s/...$//g" ^|^
@@ -9,4 +10,10 @@ for /f "tokens=* USEBACKQ" %%a in (
 set result=%%a
 )
 
-git checkout -b "wh/%result%"
+git checkout -b "%BRANCHPREFIX%/%result%"
+GOTO END
+:NOBRANCHPREFIX
+@ECHO ON
+
+@ECHO The BRANCHPREFIX environment variable was not found
+:END

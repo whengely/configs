cf0970d4563b570c938e80d56d836de36d0ba17b
Walt Hengely <whengely@igsenergy.com>
2018-08-06 11:27:49 -0400

    make it all lowercase

--- terjira/createbranch.cmd
+++ terjira/createbranch.cmd
@@ -3,7 +3,7 @@
 for /f "tokens=* USEBACKQ" %%a in (
 `jira issue %1 ^| head -n 4 ^| tail -n 3 ^| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" ^|^
  tr -s " " ^| cut -c 5- ^| sed -e "s/...$//g" ^|^
- tr -d "\r" ^| tr -d "\n" ^| cut -d " " -f1^,5- ^|^
+ tr -d "\r" ^| tr -d "\n" ^| cut -d " " -f1^,5- ^| tr "[:upper:]" "[:lower:]" ^|^
  tr -d ":" ^| sed -e "s/[[:space:]]/-/g" ^| tr -s "-" ^| sed -e "s/-$//g"`
 ) do (
 set result=%%a

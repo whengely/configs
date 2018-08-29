ec17c63c5a9c6bf5f7f7f376ecb37e38535cadea
Walt Hengely <whengely@igsenergy.com>
2018-08-29 14:13:38 -0400

    make it so it opens in a web browser if necessary

--- terjira/viewticket.sh
+++ terjira/viewticket.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 jiraissue.sh $1
-jiraurl.sh $1
+
+while true; do
+  read -p "Type Y to open this ticket in a browser? " result
+  case $result in
+    [Yy]* ) jira issue open $1; break;;
+    * ) break;
+  esac
+done

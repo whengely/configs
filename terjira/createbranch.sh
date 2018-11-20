4c3c9f9487d9ad0d84c4f470f824e30f0d2b60ea
Walt Hengely <whengely@igsenergy.com>
2018-11-20 10:58:46 -0500

    cleaned up naming logic

--- terjira/createbranch.sh
+++ terjira/createbranch.sh
@@ -4,11 +4,18 @@ if [[ -z "$BRANCHPREFIX" ]]; then
   echo 'The BRANCHPREFIX environment variable was not found'
   exit 1
 else
-  RESULT=`jira issue $1 | head -n 4 | tail -n 3 | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" |
-    tr -s " " | cut -c 5- | sed -e "s/...$//g" |
-    tr -d "\r" | tr -d "\n" | cut -d " " -f1,5- | tr "[:upper:]" "[:lower:]" |
-    tr -d ":" | tr -d "<" | tr -d ">" | tr -d "(" | tr -d ")" |
-    sed -e "s/[[:space:]]/-/g" | tr -s "-" | sed -e "s/-$//g"`
+  RESULT=`jira issue $1 |
+    head -n 4 |
+    tail -n 1 |
+    sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" |
+    cut -c 5- | 
+    sed -e "s/..$//g" |
+    tr "[:upper:]" "[:lower:]" |
+    sed -e "s/[\(\)<>-\:]/ /g" | #Replace these characters with spaces
+    sed -e "s/[']//g" | #Replace these characters with nothing
+    tr -s " " | #Collapse spaces
+    sed -e "s/[[:space:]]/-/g" | #Convert spaces to -
+    sed -e "s/-$//g"` #Remove last - if at end of line
 fi
 
-`git checkout -b $BRANCHPREFIX/$RESULT`
+git checkout -b $BRANCHPREFIX/$1-$RESULT

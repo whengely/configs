a2f9ee937c39b21f014dda15af550178ba086958
Walt Hengely <whengely@igsenergy.com>
2019-01-28 14:10:36 -0500

    link all the files in linux

--- link_linux_files.sh
+++ link_linux_files.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+sudo apt update
+sudo apt install dos2unix
 
+dos2unix ~/.zshrc
 ln -f ./zshrc ~/.zshrc
+dos2unix ~/.gitconfig
 ln -f ./gitconfig ~/.gitconfig
+dos2unix ~/.vimrc
 ln -f ./vimrc ~/.vimrc
